ba3e6134413ce8e261301483cb55022cf6a9d7df
Ian Butterworth <i.r.butterworth@gmail.com>
2019-05-09 20:41:48 -0400

    it actually records..

--- src/SpinnakerGUI.jl
+++ src/SpinnakerGUI.jl
@@ -5,6 +5,7 @@ include("utils.jl")
 # camera settings
 include("camera-settings.jl")
 cam = nothing
+sessionStat = sessionStatus()
 camSettings = settings()
 camSettingsLimits = settingsLimits()
 camGPIO = GPIO()
@@ -20,7 +21,7 @@ end
 
 # global image buffers
 camImage = nothing
-camImageFrameBuffer = nothing
+camImageFrameBuffer = Vector{Array{UInt8}}(undef,0)
 
 # GUI settings
 gui_open = nothing
@@ -29,9 +30,8 @@ control_open = true
 # performance reporting
 perfGrabFramerate = 0.0
 
-
-
 include("gui.jl")
+include("recording.jl")
 
 function start(;camid::Int64=0)
     global cam, gui_open
@@ -46,6 +46,10 @@ function start(;camid::Int64=0)
     # Start settings updater (operates asynchronously at at ~10 FPS)
     @info "Starting Camera Settings Updater (async)"
     @async_errhandle camSettingsUpdater(timerInterval=1/10)
+
+    # Start recording listener
+    @info "Starting recording listener (async)"
+    @async_errhandle videowritelistener()
 
     # Run camera control with priority
     @info "Starting Camera Acquisition"
---
+++ src/recording.jl
@@ -0,0 +1,52 @@
+using VideoIO, Dates
+
+
+```
+videowritelistener(;compression=0,overwrite=false, fps=60.0, options=``)
+Write video to file
+
+Based on https://discourse.julialang.org/t/creating-a-video-from-a-stack-of-images/646/8
+Reference for H.264: https://trac.ffmpeg.org/wiki/Encode/H.264#LosslessH.264
+The range of the compression (CRF) scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible
+```
+function videowritelistener(;compression=0,overwrite=false, fps=60.0, options=``)
+
+    ow = overwrite ? `-y` : `-n`
+    crf = compression
+    preset = compression == 0 ? "ultrafast" : "medium"
+    ffmpegpath = VideoIO.ffmpeg
+
+    global sessionStat
+    global camImageFrameBuffer
+
+    while gui_open
+        if length(camImageFrameBuffer) > 0
+            sessionStat.savedframes = 0
+            fname = string(Dates.format(now(), "yyyy-mm-dd_HHMMSS"),".avi")
+            h, w = size(camImageFrameBuffer[1])
+            withenv("LD_LIBRARY_PATH" => joinpath(dirname(dirname(pathof(VideoIO))),"deps/usr/bin/")) do
+                open(`$ffmpegpath
+                    -loglevel warning
+                    $ow -f rawvideo -pix_fmt gray -s:v $(h)x$(w)
+                    -r $fps -i pipe:0 -c:v libx264 -preset: $preset
+                    -crf $crf -pix_fmt yuv422p
+                    $options -vf "transpose=1,pad=ceil($w/2)\*2:ceil($h/2)\*2"
+                    $fname`, "w") do out
+                    while length(camImageFrameBuffer) > 0 || sessionStat.recording
+                        while length(camImageFrameBuffer) == 0 && sessionStat.recording
+                            yield() #wait without blocking during recording time
+                        end
+                        if length(camImageFrameBuffer) >0
+                            write(out, camImageFrameBuffer[1])
+                            deleteat!(camImageFrameBuffer,1)
+                        end
+                        sessionStat.bufferedframes = length(camImageFrameBuffer)
+                        sessionStat.savedframes += 1
+                        yield()
+                    end
+                end
+            end
+        end
+        yield()
+    end
+end
